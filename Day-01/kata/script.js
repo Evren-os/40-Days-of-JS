39bdd52fd9c35375b2069a2067b9d0eae711f820
Evren-os <evrnos@proton.me>
2025-02-17 03:25:06 +0600

    feat(assignments): add Day 01 kata and Day 02 code with assignments

---
+++ Day-01/kata/script.js
@@ -0,0 +1,8 @@
+console.log("Hellp, tapaScript");
+console.log("Welcome to 40 Days of JavaScript!");
+document.write("Check the browser console for a message!");
+
+document.getElementById("desc").innerHTML =
+  "Kata’s like a japanese term for assignments, mostly used in coding dojos";
+
+// document.write() executes after document.getElementByID(). Why ?
---
+++ Day-02/kata/script.js
@@ -0,0 +1,19 @@
+const personName = "Elliot Alderson";
+const age = 39;
+const isStudent = false;
+const favProgrammingLanguage = "Python";
+
+console.log(personName, age, isStudent, favProgrammingLanguage);
+
+let elliotIsKnownFor = [
+  "Five/Nice Hack",
+  "fsociety Operations",
+  "Social engineering mastery",
+  "fighting corporate oligarchy",
+];
+
+let elliotPet = {
+  name: "Flipper",
+  species: "dog",
+  origin: "rescued from Lenny",
+};

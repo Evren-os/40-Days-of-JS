527276421a10f66fdc4cc7aa5c63c68ef96cd235
Evren-os <evrnos@proton.me>
2025-02-23 10:32:25 +0600

    docs(lessons): add covered topics for days 1 and 2

--- Day-01/script.js
+++ Day-01/script.js
@@ -1,1 +1,18 @@
+/*
+Covered Topics:
+- Operands, Operators, Expressions
+- Different Types of Operators
+- Arithmetic Operators
+- Assignment Operators
+- Comparison Operators
+- Logical Operators
+- Conditional (ternary) Operator
+- Bitwise Operators
+- Relational Operators
+- Grouping and Precedence
+- typeof Operator
+- instanceof Operator
+- Task Assignments
+*/
+
 console.log("Hello from Node.js");
